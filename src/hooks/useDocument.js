1d97d0c602bea22ddf4e62d3ac93a08fd6217ce0
GYUYOUNG LEE <lee00777@algonquinlive.com>
2021-12-28 18:44:50 -0500

    Added useDocument custom hook to fetch a single realtime data document

---
+++ src/hooks/useDocument.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react"
+import { projectFirestore } from "../firebase/config"
+
+// fire store에서 collection에 있는 **single** doc REAL TIME으로 가져오기
+export const useDocument = (collection, id) => {
+  const [document, setDocument] = useState(null)
+  const [error, setError] = useState(null)
+
+  useEffect(()=>{
+    const ref = projectFirestore.collection(collection).doc(id)
+    const unsubscribe = ref.onSnapshot(snapshot=>{
+      setDocument({...snapshot.data(), id:snapshot.id})
+      setError(null)
+    }, (err) => {
+      console.log(err.message)
+      setError('Failed to get document')
+    })
+    return(()=>{ unsubscribe() })
+  },[collection, id])
+
+  return { error, document}
+}
--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import './Project.css'
+import { useDocument } from '../../hooks/useDocument'
 
 export default function Project() {
+  const {document, error} = useDocument()
   return (
     <div>
       Project
