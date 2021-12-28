0c5a5ffd33e7a296331e3a34b289759a5c0f485d
GYUYOUNG LEE <lee00777@algonquinlive.com>
2021-12-27 22:48:31 -0500

    Added signup related functionalities

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -6,23 +6,23 @@ import './Sidebar.css'
 
 export default function Sidebar() {
   return (
-    <div className='sidebar'>
-      <div className='sidebar-content'>
-        <div className='user'>
-          {/* avatar img and username */}
-          <p>Hey GY</p>
-        </div>
-        <nav className='links'>
+    <div className="sidebar">
+      <div className="sidebar-content">
+        <div className="user">
+          {/* avatar & username here later */}
+          <p>Hey user</p>  
+        </div>  
+        <nav className="links">
           <ul>
             <li>
-              <NavLink to="/">
-                <img src={DashboardIcon} alt="dashboard icon"/>
+              <NavLink exact to="/">
+                <img src={DashboardIcon} alt="dashboard icon" />
                 <span>Dashboard</span>
               </NavLink>
             </li>
             <li>
               <NavLink to="/create">
-                <img src={AddIcon} alt="add project icon"/>
+                <img src={AddIcon} alt="add project icon" />
                 <span>New Project</span>
               </NavLink>
             </li>
--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,11 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Signup.css'
 
 
 export default function Signup() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [displayName, setDisplayName] = useState('')
+  const [thumbnail, setThumbnail] = useState(null)
+  const [thumbnailError, setThumbnailError] = useState(null)
+
+  function handleFileChange(ev){
+    setThumbnail(null); // 이미 선택해놓고 다른 이미지 새롭게 선택할때 등을 대비해서, null로 초기화하고 시작하기
+    let selected = ev.target.files[0]  // "ev.target.files" returns an array by default for the case allowing multiple files for users, but here we are not going to allow selecting multiple files. Therefore just use [0].
+    console.log('selected file : ', selected)
+
+    // .name, .size, .type 이용해서 사진파일 format만 allow하고, 사이즈 제한하기
+    if(!selected){
+      setThumbnailError("Please select a file");
+      return;
+    }
+    if(!selected.type.includes('image')){
+      setThumbnailError("Selected file must be an image");
+      return;
+    }
+    if(selected.size > 100000 ){
+      setThumbnailError("Image file size must be less than 100kb");
+      return;
+    }
+    console.log("TESTTTTTTT")
+    setThumbnailError(null)
+    setThumbnail(selected)
+  }
+
+  function handleSubmit(ev){
+    ev.preventDefault();
+    console.log(email, password, displayName, thumbnail)
+  }
+
   return (
-    <div>
-      Signup
-    </div>
+    <form className='auth-form' onSubmit={handleSubmit}>
+      <h2>Sign up</h2>
+      <label>
+        <span>Email : </span>
+        <input type="email" required value={email} onChange={(ev)=>{setEmail(ev.target.value)}}/>
+      </label>
+      <label>
+        <span>Password : </span>
+        <input type="password" required value={password} onChange={(ev)=>{setPassword(ev.target.value)}}/>
+      </label>
+      <label>
+        <span>Display name : </span>
+        <input type="text" required value={displayName} onChange={(ev)=>{setDisplayName(ev.target.value)}}/>
+      </label>
+      <label>
+        <span>Profile thumbnail : </span>
+        <input type="file" required onChange={handleFileChange}/>
+        { thumbnailError && <div className='error'>{thumbnailError}</div>}
+      </label>
+      <button className='btn'>Sign up</button>
+    </form>
   )
 }
