93935bd8e557b7144ade1af2c251ed2dc5a3b290
GYUYOUNG LEE <lee00777@algonquinlive.com>
2021-12-28 17:18:47 -0500

    Implemented error handling features

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,5 +1,15 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import Select from 'react-select'
+import { useCollection } from '../../hooks/useCollection'
 import './Create.css'
+
+// 안바뀔애들은 function밖에다가 넣어주면됨.
+const categories = [
+  { value: 'development', label: 'Development' },
+  { value: 'design', label: 'Design' },
+  { value: 'sales', label: 'Sales' },
+  { value: 'marketing', label: 'Marketing' },
+]
 
 export default function Create() {
   const [name, setName] = useState('')
@@ -7,10 +17,33 @@ export default function Create() {
   const [dueDate, setDueDate] = useState('')
   const [category, setCategory] = useState('')
   const [assignedUsers, setAssignedUsers] = useState([])
+  const { documents } = useCollection('users')
+  const [ users, setUsers] = useState([]);
+  const [formError, setFormError] = useState(null)
+
+  useEffect(()=>{
+    if(documents){
+      const options = documents.map(user=>{
+        return {value:user, label:user.displayName}
+      })
+      setUsers(options)
+    }
+  },[documents])
 
   function handleSubmit(ev){
     ev.preventDefault()
-    console.log(name,details,dueDate)
+    setFormError(null)
+    if( !category ){
+      setFormError('Please select a project category')
+      return;
+    }
+    if( assignedUsers.length < 1 ){
+      setFormError('Please assign the project to at least 1 user')
+      return;
+    }
+
+
+    console.log(name,details,dueDate, category.value, assignedUsers)
   }
 
   return (
@@ -32,15 +65,18 @@ export default function Create() {
 
         <label>
           <span>Project category:</span>
-          {/* select here later */}
+          {/* npm install react-select 사용할건데, options에는 [{label: xx, value:zz},{label:aa, value:bb}]이런형태로 넣어준다.*/}
+          <Select options={categories} onChange={(option)=>{setCategory(option)}}/>
         </label>
 
         <label>
           <span>Assign to:</span>
           {/* select here later */}
+          <Select options={users} onChange={(option) => setAssignedUsers(option)} isMulti/>
         </label>
 
         <button className="btn">Add Project</button>
+        { formError && <p className='error'>{formError}</p> }
       </form>
     </div>
   )
