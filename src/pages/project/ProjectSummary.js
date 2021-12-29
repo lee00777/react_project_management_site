cfb606cc13b42479b772162cf0d602122b0f7429
GYUYOUNG LEE <lee00777@algonquinlive.com>
2021-12-28 23:07:25 -0500

    Implemented to give an authority to delete the project to creators only

--- src/pages/project/ProjectSummary.js
+++ src/pages/project/ProjectSummary.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import Avatar from '../../components/Avatar'
+import { useFirestore } from '../../hooks/useFirestore'
+import { useAuthContext } from '../../hooks/useAuthContext'
 
 export default function ProjectSummary({project}) {
+  const { deleteDocument } = useFirestore('projects')
+  const { user } = useAuthContext()
+
+  function handleClick(ev){
+    deleteDocument(project.id)
+  }
   return (
     <div>
       <div className='project-summary'>
         <h2 className='page-title'>{project.name}</h2>
+        <p>By {project.createdBy.displayName}</p>
         <p className='due-date'>
           Project due by {project.dueDate.toDate().toDateString()}
         </p>
@@ -19,6 +28,8 @@ export default function ProjectSummary({project}) {
           })}
         </div>
       </div>
+      {/* giving an authority to delete the project to creators ONLY */}
+      { project.createdBy.id === user.uid &&  <button className='btn' onClick={handleClick}>Mark as Complete</button> }
     </div>
   )
 }
